e69f51797dbbc8cbd057b0d1ce7c9169d986bd15
Damien Gavard <damien@viscaweb.com>
2016-09-01 23:00:07 +0200

    Set permissions

--- xcode_ramdisk.sh
+++ xcode_ramdisk.sh
@@ -14,10 +14,10 @@ DEV_IPHONE_DIR="/Users/$USERNAME/Library/Application Support/iPhone Simulator"
 SYS_CACHES_DIR="/Library/Caches" # this must laster than USER_CACHES_DIR...
 
 # Create folder if not exists
-mkdir -p "$USER_CACHES_DIR"
-mkdir -p "$DEV_CACHES_DIR"
-mkdir -p "$DEV_IPHONE_DIR"
-mkdir -p "$SYS_CACHES_DIR"
+mkdir -p -m 0777 "$USER_CACHES_DIR"
+mkdir -p -m 0777 "$DEV_CACHES_DIR"
+mkdir -p -m 0777 "$DEV_IPHONE_DIR"
+mkdir -p -m 0777 "$SYS_CACHES_DIR"
 
 RAMDisk() {
 	mntpt="$1"
